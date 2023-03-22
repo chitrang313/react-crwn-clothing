b1bd0b4c62e2024d253b37b60f9faf57a5564ab3
chitrang <chitrang.patel@ncoregames.com>
2023-03-22 17:26:43 +0530

    categroies display basic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 function App() {
-  return <div>hello</div>;
+  
+  const categories = [
+    {
+      id:0,
+      title:"jacket"
+    },
+    {
+      id:1,
+      title:"snikers"
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map((c) => (
+        <div className="category-container">
+          <div className="background-image"/>
+          <div className="catefory-body-container">
+            <h2>{c.title}</h2>
+            <p>shop now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
